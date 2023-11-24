a92436692c26e9c0f11ebe5cbc315b38d303df42
Didier Hartong <info@focuswider.com>
2023-11-24 09:58:46 +0100

    chore: project init

---
+++ android/app/src/main/java/new/ionic/v7/MainActivity.java
@@ -0,0 +1,5 @@
+package new.ionic.v7;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'new.ionic.v7',
+  appName: 'new-ionic-7',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
